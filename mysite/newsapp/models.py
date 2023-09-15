ddd784680696795b314f6abb413e7ac3cb91cf73
Rafał Mokrzycki <rafalmokrzycki1@gmail.com>
2023-09-15 22:40:14 +0200

    add photos display to django

--- mysite/newsapp/models.py
+++ mysite/newsapp/models.py
@@ -7,10 +7,10 @@ from django.db import models
 
 
 class Article(models.Model):
-    article_text = models.CharField(max_length=3000)
     headline = models.CharField(max_length=200)
-    uri = models.URLField()
-    pub_date = models.DateTimeField("date published")
+    article_text = models.CharField(max_length=8000)
+    image = models.ImageField(upload_to="images/")
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.article_text
--- mysite/newsapp/urls.py
+++ mysite/newsapp/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.conf import settings  # new
+from django.conf.urls.static import static  # new
+from django.urls import path  # new
 
 from . import views
 
@@ -10,3 +12,5 @@ urlpatterns = [
     # ex: /newsapp/5/
     path("<int:article_id>/", views.detail, name="detail"),
 ]
+if settings.DEBUG:  # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
